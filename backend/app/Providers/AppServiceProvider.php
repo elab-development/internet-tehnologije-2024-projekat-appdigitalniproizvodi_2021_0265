26c038efe65224d131a3b76014adda0f16f34459
jeja <jn20210265@student.fon.bg.ac.rs>
2025-10-07 17:15:20 +0200

    dodat console.php file

--- backend/app/Providers/AppServiceProvider.php
+++ backend/app/Providers/AppServiceProvider.php
@@ -6,9 +6,7 @@ use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
+    
     public function register(): void
     {
         $this->app->bind(\App\Services\ImageUploadService::class, function ($app) {
@@ -16,9 +14,7 @@ class AppServiceProvider extends ServiceProvider
         });
     }
 
-    /**
-     * Bootstrap any application services.
-     */
+    
     public function boot(): void
     {
         //
